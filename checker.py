ee1153dbe9f58d38b5e83144a3d7931134fadc06
beegeiger <beegeiger@gmail.com>
2023-01-30 10:25:07 -0800

    Added function to identify the races

--- checker.py
+++ checker.py
@@ -17,4 +17,20 @@ with open('votes.csv') as csv_file:
 
 input = [race_line, candidate_line, all_votes]
 
-def check_races(input[0]):
+def check_races(race_line):
+    """Outputs Dictionary where race["specific race"] = ["first index of race", "last index of race"] from race_line list"""
+    races = {}
+    start_index = 99999
+    current_race = ""
+    for ind, column in enumerate(race_line[9:]):
+        cell = column.split("(RCV)")
+        race = cell[0]
+        if race != "current_race":
+            if len(races)==0:
+                current_race = race
+                start_index = ind
+            else:
+                races[curent_race] = [start_index, ind - 1]
+                current_race = races
+                start_index = ind
+    return races
